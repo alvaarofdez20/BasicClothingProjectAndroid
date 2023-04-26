5896e959197ac0dd30559b8c20df4f79f5f639bf
alvaarofdez20 <alvaarofdez20@gmail.com>
2023-04-26 11:02:27 +0200

    26/04 Ventana Login acabado + Register

--- app/src/main/java/com/example/basicclothingproject/MainActivity.java
+++ app/src/main/java/com/example/basicclothingproject/MainActivity.java
@@ -1,49 +1,89 @@
 package com.example.basicclothingproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin;
-    Button btnRegister;
+    TextView textViewRegister;
+    EditText editTextEmail;
+    EditText editTextPassword;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.btnLogin);
-        btnRegister = findViewById(R.id.btnRegister);
+        textViewRegister = findViewById(R.id.textViewRegister);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
+                iniciarSesion("http://10.0.0.30/basic_clothing/login.php");
             }
         });
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                register();
+                Intent intent = new Intent(getApplicationContext(), ventanaRegister.class);
+                startActivity(intent);
             }
         });
     }
 
-    public void login(){
-        Intent login = new Intent(this, ventanaLogin.class);
-        startActivity(login);
-        finish();
-    }
-
-    public void register(){
-        Intent register = new Intent(this, ventanaRegister.class);
-        startActivity(register);
-        finish();
+    private void iniciarSesion(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()) {
+                    Intent intent = new Intent(getApplicationContext(), Tienda.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Email y/o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("email", editTextEmail.getText().toString());
+                parametros.put("password", editTextPassword.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
--- app/src/main/java/com/example/basicclothingproject/Tienda.java
+++ app/src/main/java/com/example/basicclothingproject/Tienda.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class principal extends AppCompatActivity {
+public class Tienda extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
+        setContentView(R.layout.activity_tienda);
     }
 }
